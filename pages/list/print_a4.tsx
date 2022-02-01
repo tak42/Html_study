655e3b83f8fb3cdc65fe1436076f02874572c9dd
tak42 <1206ookubo@gmail.com>
2022-02-01 19:16:30 +0900

    帳票サンプル通りに作成完了

--- pages/list/print_a4.tsx
+++ pages/list/print_a4.tsx
@@ -14,17 +14,34 @@ const Printcontents = styled.html`
   }
 
   body {
-    width: 200mm; /* 用紙の横幅を改めて指定 chrome用 */
+    width: 210mm;
+    background: #eee;
+    -webkit-print-color-adjust: exact;
+    color-adjust: exact;
+    line-height: 1.5em;
+  }
+  /* プレビュー用のスタイル */
+  @media screen {
+    body {
+      background: #eee;
+    }
   }
 `
 
 const Sectionpage = styled.section`
-  width: 200mm; /* 用紙の横幅を改めて指定 */
+  width: 210mm; /* 用紙の横幅を改めて指定 */
   height: 295mm; /* 高さには0.5mm余裕を持たせる */
   box-sizing: border-box;
   page-break-after: always; /* 改ページを行う */
-  padding: 20 25;
+  padding: 20mm 25mm;
   font-size: 11pt;
+
+  /* プレビュー用のスタイル */
+  @media screen {
+    background: white; /* 背景を白く */
+    box-shadow: 0 0.5mm 2mm rgba(0, 0, 0, 0.3); /* ドロップシャドウ */
+    margin: 5mm;
+  }
 `
 
 const Caption = styled.p`
@@ -39,6 +56,9 @@ const Destname = styled.p`
 
 const Title = styled.h1`
   text-align: center;
+  margin: 1em 0;
+  font-size: 16pt;
+  letter-spacing: 0.2em;
 `
 
 const Price = styled.dl`
@@ -57,6 +77,25 @@ const Pricedt = styled.dt`
 const Pricedd = styled.dd`
   padding: 1em;
 `
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+
+  th,
+  td {
+    padding: 0.1em;
+    border: solid 0.5pt #000;
+  }
+
+  thead,
+  tfoot th {
+    background: #ccc;
+  }
+
+  tfoot td:empty {
+    border: none;
+  }
+`
 
 const No = styled.th`
   width: 30px;
@@ -69,6 +108,10 @@ const Cost = styled.th`
 `
 const Sum = styled.th`
   width: 150px;
+`
+
+const Bank = styled.p`
+  margin-bottom: 1em;
 `
 const Home: NextPage = () => {
   return (
@@ -96,7 +139,7 @@ const Home: NextPage = () => {
             <Pricedt> ご請求金額 </Pricedt>
             <Pricedd> 10,000円 </Pricedd>
           </Price>
-          <table>
+          <Table>
             <thead>
               <tr>
                 <No>NO</No>
@@ -181,14 +224,14 @@ const Home: NextPage = () => {
                 <td>10,000円</td>
               </tr>
             </tfoot>
-          </table>
-          <p className="bank">
+          </Table>
+          <Bank>
             銀行名：○○銀行 ○○支店
             <br />
             口座番号：普通 000000
             <br />
             口座名義：〇×□△
-          </p>
+          </Bank>
           <p>以上、よろしくお願い致します。</p>
         </Sectionpage>
       </body>
